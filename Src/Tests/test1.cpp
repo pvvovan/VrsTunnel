9eed976e26e49f8a370d3edf1d643b72a2b04272
PVVovan <pvvovan@gmail.com>
2019-07-24 16:00:40 +0300

    test template

---
+++ Src/Tests/test1.cpp
@@ -0,0 +1,32 @@
+
+#include <future>
+#include <CppUTest/TestHarness.h>
+// // #include <CppUTestExt/MockSupport.h>
+
+
+#include <string>
+#include <thread>
+
+
+TEST_GROUP(FirstTestGroup)
+{
+};
+
+TEST(FirstTestGroup, FirstTest)
+{
+    double d = 1;
+    CHECK_EQUAL(1, d);
+};
+
+
+TEST_GROUP(ListTestGroup)
+{
+    void setup()
+    {
+        
+    }
+    void teardown()
+    {
+        
+    }
+};

2643acb1f14f21a5832ad87d155420b7ffbe7fea
Volodymyr Petrovskyy <pvvovan@gmail.com>
2025-11-01 05:46:46 -0700

    Test NtripClientVm

---
+++ dashb/test/UnitTest_NtripClientVm.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using dashb.Models;
+using dashb.ViewModels;
+
+namespace test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test_NtripServerVm()
+    {
+        NtripClient client = new("Client 1", "Pw1", Guid.NewGuid());
+        RelayCommand<NtripClientVm> stubCmd = new(p => { });
+        NtripClientVm clientVm = new(stubCmd, stubCmd, stubCmd, client);
+        Assert.Equal(clientVm.Name, client.Name);
+    }
+}

4dac6f0808926a9321aec52de17239ac96a9098d
Volodymyr Petrovskyy <pvvovan@gmail.com>
2025-12-21 10:05:20 +0100

    Remove condition COV

--- covbuild.sh
+++ covbuild.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(dirname "$(readlink -f $0)")
 
-cmake -S ${SCRIPT_DIR}/Src -B ${SCRIPT_DIR}/build -D COV=ON
+cmake -S ${SCRIPT_DIR}/Src -B ${SCRIPT_DIR}/build
 cmake --build ${SCRIPT_DIR}/build --verbose --parallel
 cmake --build ${SCRIPT_DIR}/build --target coverage

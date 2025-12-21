575a9f8a05a2c54290dadba1b9fa39b1547e9518
Volodymyr Petrovskyy <pvvovan@gmail.com>
2025-12-21 08:59:33 +0100

    Measure coverage

---
+++ covbuild.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname "$(readlink -f $0)")
+
+cmake -S ${SCRIPT_DIR}/Src -B ${SCRIPT_DIR}/build -D COV=ON
+cmake --build ${SCRIPT_DIR}/build --verbose --parallel
+cmake --build ${SCRIPT_DIR}/build --target coverage

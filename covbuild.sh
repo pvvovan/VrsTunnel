e26fff61e359ae64c2db1aab4a5ca6d1b50e7b02
PVVovan <pvvovan@gmail.com>
2026-01-05 12:50:13 +0100

    Select ninja as cmake generator

--- covbuild.sh
+++ covbuild.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$(dirname "$(readlink -f $0)")
 
-cmake -S ${SCRIPT_DIR}/Src -B ${SCRIPT_DIR}/build &&
+cmake -S ${SCRIPT_DIR}/Src -B ${SCRIPT_DIR}/build -G Ninja &&
 cmake --build ${SCRIPT_DIR}/build --verbose --parallel &&
 cmake --build ${SCRIPT_DIR}/build --target coverage

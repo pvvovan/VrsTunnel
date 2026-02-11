b8acfabb77449740d47d5896e4a50532815816f9
PVVovan <pvvovan@gmail.com>
2026-02-11 06:51:02 +0100

    Remove cov.sh

--- dashb/test/run.sh
+++ dashb/test/run.sh
@@ -3,4 +3,13 @@
 SCRIPT_DIR=$(dirname "$(readlink -f $0)")
 cd ${SCRIPT_DIR}
 
+find ${SCRIPT_DIR}/TestResults/ -name "*.cobertura.xml" | xargs -n1 rm
+
 dotnet test --logger html --collect:"XPlat Code Coverage"
+
+COVDATA=$(find ${SCRIPT_DIR}/TestResults/ -name "*.cobertura.xml")
+
+reportgenerator \
+-reports:"${COVDATA}" \
+-targetdir:"TestResults/coveragereport" \
+-reporttypes: Html

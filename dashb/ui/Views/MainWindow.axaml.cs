0d20bd13074a91ae63fe547a05f87833e683ec52
PVVovan <pvvovan@gmail.com>
2026-02-28 11:35:01 +0100

    Create menu

--- dashb/ui/Views/MainWindow.axaml.cs
+++ dashb/ui/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
+using System.IO;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 using vm.ViewModels;
 
 namespace ui.Views;
@@ -18,5 +22,35 @@ public partial class MainWindow : Window, IDialog
         };
         inputVm.Wnd = inputView;
         inputView.ShowDialog(this);
+    }
+
+    private bool dialogOpen = false;
+
+    private async void Button_Clicked(object sender, RoutedEventArgs args)
+    {
+        if (dialogOpen)
+        {
+            return;
+        }
+        dialogOpen = true;
+
+        // Get top level from the current control. Alternatively, you can use Window reference instead.
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        // Start async operation to open the dialog.
+        var file = await topLevel!.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+        {
+            Title = "Save Text File"
+        });
+
+        if (file is not null)
+        {
+            // Open writing stream from the file.
+            await using var stream = await file.OpenWriteAsync();
+            using var streamWriter = new StreamWriter(stream);
+            // Write some content to the file.
+            await streamWriter.WriteLineAsync("Hello World!");
+        }
+        dialogOpen = false;
     }
 }

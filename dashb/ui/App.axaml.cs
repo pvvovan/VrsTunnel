d3224edfd0770ffc890b9273e17bdd334d00c482
PVVovan <pvvovan@gmail.com>
2026-04-29 06:23:58 +0200

    Upgrade Avalonia to version 12.0.1

--- dashb/ui/App.axaml.cs
+++ dashb/ui/App.axaml.cs
@@ -21,9 +21,6 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
-            // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
-            DisableAvaloniaDataAnnotationValidation();
             desktop.MainWindow = new MainWindow();
             var mainVm = new MainWindowVm((IDialog)desktop.MainWindow, new JsonConfig());
             desktop.MainWindow.DataContext = mainVm;
@@ -39,18 +36,5 @@ public partial class App : Application
         }
 
         base.OnFrameworkInitializationCompleted();
-    }
-
-    private void DisableAvaloniaDataAnnotationValidation()
-    {
-        // Get an array of plugins to remove
-        var dataValidationPluginsToRemove =
-            BindingPlugins.DataValidators.OfType<DataAnnotationsValidationPlugin>().ToArray();
-
-        // remove each entry found
-        foreach (var plugin in dataValidationPluginsToRemove)
-        {
-            BindingPlugins.DataValidators.Remove(plugin);
-        }
     }
 }
